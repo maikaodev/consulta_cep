55dfb125cb9d2bc5c35a430489ec0de3ed69bb63
maikaodev <maikao.dev@gmail.com>
2022-08-16 18:28:05 -0300

    Criando layout

---
+++ src/App/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <Form />
+</template>
+
+<script>
+import Form from "../components/Form/Form.vue";
+export default {
+  components: { Form },
+};
+</script>
+
+<style src="./style.sass" lang="sass" />
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,39 @@
+<template>
+  <main class="container">
+    <form class="container__form">
+      <Input class="container__form__text" />
+      <Input class="container__form__submit" type="submit" value="Buscar CEP" />
+    </form>
+    <section class="container__result">
+      <ul class="container__result__list">
+        <li class="container__result__list__item">
+          CEP:
+          <span>12345678</span>
+        </li>
+        <li class="container__result__list__item">
+          Cidade:
+          <span>Alagoas</span>
+        </li>
+        <li class="container__result__list__item">
+          Cidade:
+          <span>Maceió</span>
+        </li>
+        <li class="container__result__list__item">
+          Logradouro:
+          <span>1° Travessa Costa Nabal </span>
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
+<script>
+import Input from "../Input/Input.vue";
+export default {
+  name: "Form",
+  components: { Input },
+  data() {
+    return {};
+  },
+};
+</script>
+<style src="./style.sass" lang="sass" scoped />
