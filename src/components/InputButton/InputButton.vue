94970626446027f7398a8fdfdc2d562adc72e1a2
maikaodev <maikao.dev@gmail.com>
2022-08-16 20:08:01 -0300

    Pegando o endereço corretamente

---
+++ src/components/InputButton/InputButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="submit" :value="value" />
+</template>
+<script>
+export default {
+  name: "InputButton",
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style></style>
