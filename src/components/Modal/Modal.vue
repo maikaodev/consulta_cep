2be7a9fcd697332bd7ab5d43f40c02989bae4e63
maikaodev <maikao.dev@gmail.com>
2022-08-18 14:14:16 -0300

    Criando modal e aplicando ela nos erros

---
+++ src/components/Modal/Modal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="modal">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: "Modal",
+  data() {
+    return {};
+  },
+};
+</script>
+<style src="./style.sass"></style>
