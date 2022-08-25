0f51f8c46e386169fcc85679ea2e960ab60c2699
maikaodev <maikao.dev@gmail.com>
2022-08-25 16:36:49 -0300

    Adicionando loading na req e corrigindo bug de repetir req

---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="background">
+    <img src="/loading.gif" alt="Loading..." />
+  </div>
+</template>
+<script>
+export default {
+  name: "Loading",
+  data() {
+    return {};
+  },
+};
+</script>
+<style src="./style.sass" lang="sass" scoped></style>
